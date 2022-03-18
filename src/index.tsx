886d54409af9bf79931248a42a5ae8b7699993c8
LyubooShv <liubomirshivachev@gmai.com>
2022-03-18 19:16:41 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-      <React.StrictMode>
+  <React.StrictMode>
     <h1>Welcome to Vite skeleton</h1>
   </React.StrictMode>,
   document.getElementById("root")
