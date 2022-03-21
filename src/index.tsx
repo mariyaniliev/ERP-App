3def8f075cf6472b777d55e746fd05fe79413a16
LyubooShv <liubomirshivachev@gmai.com>
2022-03-21 17:06:38 +0200

    add router v6 components in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import Counter from "./Counter";
@@ -12,8 +13,13 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <h1>Welcome to Vite skeleton</h1>
-        <Counter />
+        <BrowserRouter>
+          <h1>Welcome to Vite skeleton</h1>
+          <Routes>
+            <Route path="counter" element={<Counter />} />
+            <Route path="/" element={<Navigate to="/counter" replace />} />
+          </Routes>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
