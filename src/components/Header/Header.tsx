31cd67dfb0a0937b3248c09c944ba2dcc8df9002
Mariyan <mapo441@gmail.com>
2022-03-24 23:21:51 +0200

    update header styles and structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,43 +1,42 @@
 import React, { useState } from "react";
 import { useAppDispatch, useAppSelector, RootState } from "../../redux/store";
-import { styled, useTheme } from "@mui/material/styles";
-import { IconButton } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { toggle } from "../../redux/reducer/drawerHeader";
 import logo from "../../theme/assets/gs-logo.png";
 import "./header.css";
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
 const HeaderComponent = () => {
   const dispatch = useAppDispatch();
   const theme = useTheme();
   const open = useAppSelector((state: RootState) => state.drawerHeader.open);
-  const [user, setUser] = useState("asdasdsa");
+  const [user, setUser] = useState("Nikolay Petkov");
+  const [role, setRole] = useState("Admin");
+
+  const menuToggleButton = open ? (
+    <ChevronLeftIcon className="sideBarButtonIcon" />
+  ) : (
+    <ChevronRightIcon className="sideBarButtonIcon" />
+  );
 
   return (
     <div className="header">
       <div className="headerButtonAndLogo">
-        <DrawerHeader>
-          <IconButton onClick={() => dispatch(toggle())}>
-            {open ? (
-              <ChevronLeftIcon className="sideBarButtonIcon" />
-            ) : (
-              <ChevronRightIcon className="sideBarButtonIcon" />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <img className="logo" src={logo} alt="Logo" />
+        <div className="menuToggleWrapper" onClick={() => dispatch(toggle())}>
+          {menuToggleButton}
+        </div>
+        <img className="logo" src={logo} alt="Generic Soft Logo" />
       </div>
-      <div className="username">User:{user}</div>
+      <div className="userDetailWrapper">
+        <div
+          className="userLogoWrapper"
+          style={{
+            background: `url(${logo})`,
+          }}
+        />
+          <p className="userName">{user}</p>
+      </div>
     </div>
   );
 };
--- src/redux/reducer/counter.ts
+++ src/redux/reducer/counter.ts
@@ -18,6 +18,7 @@ export const counterSlice = createSlice({
   },
 });
 
+
 export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
