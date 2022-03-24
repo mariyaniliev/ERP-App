cb346428b0da57ec6b40fe40d0f74cda6c8ee199
Georgi07 <g.nikolaev@abv.bg>
2022-03-24 15:05:41 +0200

    Changed default theme colors

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { THEME_FONT_FAMILY } from "../../theme/theme-constants";
+
 const Home = () => {
   return (
     <div>
       HOME
-      <h2 style={{ fontFamily: `${THEME_FONT_FAMILY}` }}>
-        Test font family in the app
-      </h2>
       {/* without Outlet component, children property doesnt work */}
       <Outlet />
     </div>
--- src/theme/theme-constants.ts
+++ src/theme/theme-constants.ts
@@ -1,11 +1,7 @@
 export const THEME_FONT_FAMILY = "'Poppins', cursive";
 
 export const THEME_COLORS = {
-  accentBlue: "var(--color-accentblue)",
-  lightblue: "var(--color-lightblue)",
-  darkblue01: "var(--color-darkblue01)",
-  darkblue02: "var(--color-darkblue02)",
-  dark: "var(--color-dark)",
-  accentpurple: "var(--color-accentpurple)",
+  primaryGradient: "var(--color-primary-gradient)",
+  aquaBlue: "var(--color-aquablue)",
   purple: "var(--color-purple)",
 } as const;
