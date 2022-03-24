0b465ebf7b950474df1e6ee19da1ddc63dbea086
Georgi07 <g.nikolaev@abv.bg>
2022-03-24 13:39:18 +0200

    Setup project theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import { routes } from "./pages/routes";
+import "./App.global.css";
 
 const App = () => {
   const element = useRoutes(routes);
---
+++ src/theme/theme-constants.ts
@@ -0,0 +1,11 @@
+export const THEME_FONT_FAMILY = "'Poppins', cursive";
+
+export const THEME_COLORS = {
+  accentBlue: "var(--color-accentblue)",
+  lightblue: "var(--color-lightblue)",
+  darkblue01: "var(--color-darkblue01)",
+  darkblue02: "var(--color-darkblue02)",
+  dark: "var(--color-dark)",
+  accentpurple: "var(--color-accentpurple)",
+  purple: "var(--color-purple)",
+} as const;
