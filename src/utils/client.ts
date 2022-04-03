935c721abfc2079516708d3782a0d28b30296b00
Ivan Kraev <ivankraev2@abv.bg>
2022-04-03 17:31:15 +0300

    transformed api client into hook

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,31 +1,35 @@
-import axios from "axios";
+import Axios from "axios";
 import { useAppSelector, RootState } from "../redux/store";
 import { userActions } from "../redux/reducer/user";
 import { get } from "lodash";
-const accessToken = useAppSelector(
-  (state: RootState) => state.user.user.accessToken
-);
-const refreshToken = useAppSelector(
-  (state: RootState) => state.user.user.refreshToken
-);
-const actions = userActions();
+export const useApiClient = () => {
+  const accessToken = useAppSelector(
+    (state: RootState) => state.user.user?.accessToken
+  );
+  const refreshToken = useAppSelector(
+    (state: RootState) => state.user.user?.refreshToken
+  );
+  const actions = userActions();
 
-const axiosInstance = axios.create({
-  baseURL: "https://localhost:4000",
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-    "x-refresh": `Bearer ${refreshToken}`,
-  },
-});
+  const apiClient = Axios.create({
+    baseURL: "http://localhost:4000",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "x-refresh": `Bearer ${refreshToken}`,
+    },
+  });
 
-axios.interceptors.response.use(
-  function (successRes) {
-    const newAcessToken = get(successRes, "x-access-token", "");
-    if (newAcessToken) {
-      actions.setAccessToken(newAcessToken);
+  apiClient.interceptors.response.use(
+    function (successRes) {
+      const newAcessToken = get(successRes, "x-access-token", "");
+      if (newAcessToken) {
+        actions.setAccessToken(newAcessToken);
+      }
+    },
+    function (error) {
+      console.log(error);
     }
-  },
-  function (error) {
-    console.log(error);
-  }
-);
+  );
+
+  return apiClient;
+};
