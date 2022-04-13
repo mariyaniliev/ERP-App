23ee8fe325c3d9768fd0661ee4b01a337eb07167
Ivan Kraev <ivankraev2@abv.bg>
2022-04-13 10:38:19 +0300

    implemented remember me functionality

---
+++ src/services/api-endpoints.ts
@@ -0,0 +1,56 @@
+enum SearchUsersProps {
+  emailOrName,
+  page,
+  limit,
+  leadId,
+  birthday,
+  startingDate,
+  timeOffRemainingDays,
+}
+
+export default {
+  users: {
+    postUser: (leadId?: string) => `/users/${leadId ? leadId : ""}`,
+    searchUsers: (
+      searchBy: SearchUsersProps,
+      searchTerm: string,
+      page = 1,
+      limit = 10
+    ) => `/users/search?${searchBy}=${searchTerm}&page=${page}&limit=${limit}`,
+    getUsers: "/users",
+    getUser: (userId: string) => {
+      `/users/${userId}`;
+    },
+    updateUser: (userId: string, leadId?: string) => {
+      `/users/${userId}/${leadId ? leadId : ""}`;
+    },
+    deleteUser: (userId: string) => {
+      `/users/${userId}`;
+    },
+  },
+  leads: {
+    postLead: (userId: string) => `/leads/${userId}`,
+    getLeads: "/leads",
+    getLead: (leadId: string) => `/leads/${leadId}`,
+    deleteLead: (leadId: string) => `/leads/${leadId}`,
+  },
+  timeOffs: {
+    postTimeOff: (userId: string) => `/timeoffs/${userId}`,
+    getTimeOffs: "/timeoffs",
+    getTimeOff: (id: string) => `/timeoffs/${id}`,
+    updateTimeOff: (id: string) => `/timeoffs/${id}`,
+    deleteLead: (id: string) => `/timeoffs/${id}`,
+  },
+  celebrations: {
+    postCelebration: (userId: string) => `/celebrations/${userId}`,
+    getCelebrations: "/celebrations",
+    getCelebration: (id: string) => `/celebrations/${id}`,
+    updateCelebration: (id: string) => `/celebrations/${id}`,
+    deleteCelebration: (id: string) => `/celebrations/${id}`,
+  },
+  sessions: {
+    postSession: "/sessions",
+    getSessions: "/sessions",
+    deleteSessions: "/sessions",
+  },
+};
