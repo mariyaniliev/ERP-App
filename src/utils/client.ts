fa50b16ac787b03dd81c0d40274eed3bd2e2fa7a
Ivan Kraev <ivankraev2@abv.bg>
2022-04-25 18:09:06 +0300

    fix

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -14,7 +14,7 @@ export const useApiClient = () => {
 
   // TODO define baseUrl in env.development and take it from there
   const apiClient = Axios.create({
-    baseURL: " https://genericsoftapi.herokuapp.com/",
+    baseURL: "https://genericsoftapi.herokuapp.com/",
     headers: {
       Authorization: `Bearer ${accessToken}`,
       "x-refresh": `Bearer ${refreshToken}`,
