5b9fc1329c0fc4cbed5e69cd36a349bb0970b556
Georgi07 <g.nikolaev@abv.bg>
2022-04-30 01:02:19 +0300

    Fixed input styles

--- src/design-system/Input/input-styles.ts
+++ src/design-system/Input/input-styles.ts
@@ -3,14 +3,16 @@ import { THEME_COLORS } from "../../theme/theme-constants";
 
 export const StyledInputElement = styled("input")({
   width: "100%",
-  height: "48px",
-  fontSize: "14px",
+  height: "18px",
+  display: "flex",
+  alignItems: "center",
+  fontSize: "12px",
   color: THEME_COLORS.grey04,
   fontFamily: "Open Sans, sans-serif",
   border: "none",
   borderRadius: "16px",
   boxShadow: `0px 3px 6px ${THEME_COLORS.grey05}`,
-  padding: "14px 23px",
+  padding: "14px 10px",
   "&:hover": {
     color: THEME_COLORS.purple,
   },
